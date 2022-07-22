cc86b75bcd8ade95f0deaacbfb657fd1c3614a12
todo-list <dinhthanhtien2k2@gmail.com>
2022-07-22 13:29:09 +0700

    Initialize and config model

---
+++ src/models/role.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Role = new Schema({
+     name: { type: String }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Role', Role)
---
+++ src/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    username: { type: String },
+    email: { type: String },
+    password: { type: String },
+    roles: [
+        { type: Schema.Types.ObjectId, ref: 'Role' }
+    ]
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', User)
