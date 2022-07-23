a64668604d4a79c00ce1d684b57cce1fc4f75044
todo-list <dinhthanhtien2k2@gmail.com>
2022-07-23 22:11:17 +0700

    Delete succesfull with _id: ... in mongdb

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,6 +10,19 @@ class UserController {
             res.status(500).json(error)
         }
     }
+
+    async delete (req, res, next) {
+        try {
+            await User.deleteOne({ _id: req.params.id })
+            .then(() => res.send(`delete succesfull with id ${req.params.id}`))
+            .catch((error) => next(error))
+            res.status(200).json('Delete succesfull.')
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
+
 }
 
 module.exports = new UserController()
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,5 +5,6 @@ const route = express.Router()
 const userController = require('../controllers/user.controller')
 
 route.get('/users', userController.getAllUser)
+route.delete('/delete/:id', userController.delete)
 
 module.exports = route
